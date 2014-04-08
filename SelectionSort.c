4072131b3c9b2cb4fb6133a6a7e46b45ff6bee69
FedeDP <nierro92@gmail.com>
2014-04-08 23:33:17 +0200

    Selection_sort better indentation

--- SelectionSort.c
+++ SelectionSort.c
@@ -5,8 +5,8 @@
 #define N 10000
 
 typedef struct list_int{
-	int x;
-	struct list_int *next;
+  int x;
+  struct list_int *next;
 }list_f;
 
 list_f *reclist(list_f *h);
@@ -29,43 +29,43 @@ return 0;
 }
 
 list_f *reclist(list_f *h){
-	if((h=malloc(sizeof(list_f)))&&(i!=N)){
-		h->x=rand()%(N*100)+1;
-		i++;
-		h->next=reclist(h->next);		
-	}else
-		h=NULL;
-	return h;				
+  if((h=malloc(sizeof(list_f)))&&(i!=N)){
+    h->x=rand()%(N*100)+1;
+    i++;
+    h->next=reclist(h->next);   
+  }else
+    h=NULL;
+  return h;       
 }
 
 void printlist(list_f *h){
-	if(h){
-		printf("\n%d",h->x);
-		return printlist(h->next);		
-	}return;
+  if(h){
+    printf("\n%d",h->x);
+    return printlist(h->next);    
+  }return;
 }
 
 void freelist(list_f *h){
-	if(h){
-		freelist(h->next);
-		free(h);
-	}return;
+  if(h){
+    freelist(h->next);
+    free(h);
+  }return;
 }
 
 void selection_sort(list_f *h){
-	list_f *temp=NULL,*min=NULL;
-	int tmp;
-	if(h->next){
-		min=h;
-		for(temp=h->next;temp;temp=temp->next){
-			if(temp->x < min->x)
-				min=temp;
-		}
-		if(min!=h){
-			tmp=h->x;
-			h->x=min->x;
-			min->x=tmp;
-		}
-		selection_sort(h->next);	
-	}return;
+  list_f *temp=NULL,*min=NULL;
+  int tmp;
+  if(h->next){
+    min=h;
+    for(temp=h->next;temp;temp=temp->next){
+      if(temp->x < min->x)
+        min=temp;
+    }
+    if(min!=h){
+      tmp=h->x;
+      h->x=min->x;
+      min->x=tmp;
+    }
+    selection_sort(h->next);  
+  }return;
 }
