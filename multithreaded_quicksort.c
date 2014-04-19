944baa6c5dccedce5238f8c28351803bc65f1ca0
FedeDP <nierro92@gmail.com>
2014-04-19 19:34:22 +0200

    updated to follow linux kernel coding style

--- multithreaded_quicksort.c
+++ multithreaded_quicksort.c
@@ -6,116 +6,125 @@
 #define N 10000
 #define MAX_THREAD 10
 
-typedef struct list_int{
-  int x;
-  struct list_int *next;
-}list_f;
+typedef struct list_int {
+	int x;
+	struct list_int *next;
+} list_f;
 
-list_f *reclist (list_f *h);
-void printlist (list_f *h);
-void freelist (list_f *h);
-void quicksort (list_f **h, list_f *end);
+list_f *reclist(list_f *h);
+void printlist(list_f *h);
+void freelist(list_f *h);
+void quicksort(list_f **h, list_f *end);
 void second_half(list_f **h);
 
 int i = 0;
 int thread_num = 0;
 
-int main()
+int main(void)
 {
-list_f *h = NULL;
-srand(time(NULL));
-h = reclist(h);
-printlist(h);
-quicksort(&h, NULL);
-printlist(h);
-freelist(h);
-return 0;
+	list_f *h = NULL;
+	srand(time(NULL));
+	h = reclist(h);
+	printlist(h);
+	quicksort(&h, NULL);
+	printlist(h);
+	freelist(h);
+	return 0;
 }
 
-list_f *reclist(list_f *h){
-  if((h = malloc(sizeof(list_f))) && (i != N)){
-    h->x = rand()%N + 1;
-    i++;
-    h->next = reclist(h->next);   
-  }else
-    h = NULL;
-  return h;       
+list_f *reclist(list_f *h)
+{
+	h = malloc(sizeof(list_f));
+	if ((h) && (i != N)) {
+		h->x = rand()%N + 1;
+		i++;
+		h->next = reclist(h->next);
+	} else {
+		h = NULL;
+	}
+	return h;
 }
 
-void printlist(list_f *h){
-  if(h){
-    printf("\n%d", h->x);
-    return printlist(h->next);    
-  }
+void printlist(list_f *h)
+{
+	if (h) {
+		printf("\n%d", h->x);
+		return printlist(h->next);
+	}
 }
 
-void freelist(list_f *h){
-  if(h){
-    freelist(h->next);
-    free(h);
-  }
+void freelist(list_f *h)
+{
+	if (h) {
+		freelist(h->next);
+		free(h);
+	}
 }
 
-void quicksort(list_f **h, list_f *end){ 
-  list_f *temp = NULL, *pivot = NULL, *tmp = NULL;
-  pthread_t second_half_thread;
-  int created=0;
-  pivot = *h;
-  for(temp = *h; temp->next != end; temp = temp->next){
-    while((temp->next != end) && (temp->next->x < pivot->x)){
-      tmp = temp->next;
-      temp->next = temp->next->next;
-      tmp->next = *h;
-      *h = tmp;
-    }
-    if(temp->next == end)
-      break;
-  }
-  if((pivot->next) && (pivot->next->next)){
-    if(thread_num < MAX_THREAD){
-      thread_num++;
-      created=1;
-      pthread_create(&second_half_thread, NULL, (void*)second_half, &pivot->next);
-    }else
-      quicksort(&pivot->next, NULL);
-  }
-  if(((*h) != pivot) && ((*h)->next != pivot))
-    quicksort(h, pivot);
-  if(created){ 
-    pthread_join(second_half_thread, NULL);
-    thread_num--;
-  }
-  return;
+void quicksort(list_f **h, list_f *end)
+{
+	list_f *temp = NULL, *pivot = NULL, *tmp = NULL;
+	pthread_t second_half_thread;
+	int created = 0;
+	pivot = *h;
+	for (temp = *h; temp->next != end; temp = temp->next) {
+		while ((temp->next != end) && (temp->next->x < pivot->x)) {
+			tmp = temp->next;
+			temp->next = temp->next->next;
+			tmp->next = *h;
+			*h = tmp;
+		}
+		if (temp->next == end)
+			break;
+	}
+	if ((pivot->next) && (pivot->next->next)) {
+		if (thread_num < MAX_THREAD) {
+			thread_num++;
+			created = 1;
+			pthread_create(&second_half_thread, NULL, (void *)second_half, &pivot->next);
+		} else {
+			quicksort(&pivot->next, NULL);
+		}
+	}
+	if (((*h) != pivot) && ((*h)->next != pivot))
+		quicksort(h, pivot);
+	if (created) {
+		pthread_join(second_half_thread, NULL);
+		thread_num--;
+	}
+	return;
 }
 
-void second_half(list_f **h){
-  list_f *temp = NULL, *pivot = NULL, *tmp = NULL;
-  pthread_t second_half_thread;
-  int created=0;
-  pivot = *h;
-  for(temp = *h; temp->next; temp = temp->next){
-    while((temp->next) && (temp->next->x < pivot->x)){
-      tmp = temp->next;
-      temp->next = temp->next->next;
-      tmp->next = *h;
-      *h = tmp;
-    }
-    if(!temp->next)
-      break;
-  }
-  if((pivot->next) && (pivot->next->next)){
-    if(thread_num < MAX_THREAD){
-      thread_num++;
-      created=1;
-      pthread_create(&second_half_thread, NULL, (void*)second_half, &pivot->next);
-    }else
-      quicksort(&pivot->next, NULL);
-  }
-  if(((*h) != pivot) && ((*h)->next != pivot))
-    quicksort(h, pivot);
-  if(created){ 
-    pthread_join(second_half_thread, NULL);
-    thread_num--;
-  }
-  return;
+void second_half(list_f **h)
+{
+	list_f *temp = NULL, *pivot = NULL, *tmp = NULL;
+	pthread_t second_half_thread;
+	int created = 0;
+	pivot = *h;
+	for (temp = *h; temp->next; temp = temp->next) {
+		while ((temp->next) && (temp->next->x < pivot->x)) {
+			tmp = temp->next;
+			temp->next = temp->next->next;
+			tmp->next = *h;
+			*h = tmp;
+		}
+		if (!temp->next)
+			break;
+	}
+	if ((pivot->next) && (pivot->next->next)) {
+		if (thread_num < MAX_THREAD) {
+			thread_num++;
+			created = 1;
+			pthread_create(&second_half_thread, NULL, (void *)second_half, &pivot->next);
+		} else {
+			quicksort(&pivot->next, NULL);
+		}
+	}
+	if (((*h) != pivot) && ((*h)->next != pivot))
+		quicksort(h, pivot);
+	if (created) {
+		pthread_join(second_half_thread, NULL);
+		thread_num--;
+	}
+	return;
 }
