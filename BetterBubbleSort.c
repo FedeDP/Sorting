74403cf6b7377ba9b171e3385081d07fcb434f86
FedeDP <nierro92@gmail.com>
2014-04-19 19:18:32 +0200

    updated to match linux kernel coding style

--- BetterBubbleSort.c
+++ BetterBubbleSort.c
@@ -4,10 +4,10 @@
 
 #define N 100000
 
-typedef struct list_int{
-  int x;
-  struct list_int *next;
-}list_f;
+typedef struct list_int {
+	int x;
+	struct list_int *next;
+} list_f;
 
 list_f *reclist(list_f *h);
 void printlist(list_f *h);
@@ -17,59 +17,67 @@ int bubblesort(list_f *h, int swap);
 
 int i = 0;
 
-int main(int argc, char *argv[])
+int main(void)
 {
-list_f *h = NULL;
-srand(time(NULL));
-h=reclist(h);
-printlist(h);
-sortlist(h);
-printlist(h);
-freelist(h);
-return 0;
+	list_f *h = NULL;
+	srand(time(NULL));
+	h = reclist(h);
+	printlist(h);
+	sortlist(h);
+	printlist(h);
+	freelist(h);
+	return 0;
 }
 
-list_f *reclist(list_f *h){
-  if((h = malloc(sizeof(list_f))) && (i!=N)){
-    h->x = rand()%N+1;
-    i++;
-    h->next = reclist(h->next);   
-  }else
-    h = NULL;
-  return h;       
+list_f *reclist(list_f *h)
+{
+	h = malloc(sizeof(list_f));
+	if ((h) && (i != N)) {
+		h->x = rand()%N+1;
+		i++;
+		h->next = reclist(h->next);
+	} else {
+		h = NULL;
+	}
+	return h;
 }
 
-void printlist(list_f *h){
-  if(h){
-    printf("\n%d", h->x);
-    return printlist(h->next);    
-  }
+void printlist(list_f *h)
+{
+	if (h) {
+		printf("\n%d", h->x);
+		return printlist(h->next);
+	}
 }
 
-void freelist(list_f *h){
-  if(h){
-    freelist(h->next);
-    free(h);
-  }
+void freelist(list_f *h)
+{
+	if (h) {
+		freelist(h->next);
+		free(h);
+	}
 }
 
-void sortlist(list_f *h){
-  list_f *temp = NULL;
-  for(temp = h->next; temp; temp = temp->next){
-    if(bubblesort(h,0) == 0)
-      break;
-  }
+void sortlist(list_f *h)
+{
+	list_f *temp = NULL;
+	for (temp = h->next; temp; temp = temp->next) {
+		if (bubblesort(h, 0) == 0)
+			break;
+	}
 }
 
-int bubblesort(list_f *h, int swap){
-  int tmp;
-  if(h->next){
-    if(h->x > h->next->x){
-      swap = 1;
-      tmp = h->next->x;
-      h->next->x = h->x;
-      h->x = tmp;
-    }
-    return bubblesort(h->next, swap);
-  }return swap;
+int bubblesort(list_f *h, int swap)
+{
+	int tmp;
+	if (h->next) {
+		if (h->x > h->next->x) {
+			swap = 1;
+			tmp = h->next->x;
+			h->next->x = h->x;
+			h->x = tmp;
+		}
+	return bubblesort(h->next, swap);
+	}
+	return swap;
 }
--- BetterBubbleSort_iterativo.c
+++ BetterBubbleSort_iterativo.c
@@ -4,10 +4,10 @@
 
 #define N 100000
 
-typedef struct list_int{
-  int x;
-  struct list_int *next;
-}list_f;
+typedef struct list_int {
+	int x;
+	struct list_int *next;
+} list_f;
 
 list_f *reclist(list_f *h);
 void printlist(list_f *h);
@@ -17,54 +17,60 @@ int bubblesort(list_f *h, int swap);
 
 int i = 0;
 
-int main()
+int main(void)
 {
-list_f *h = NULL;
-srand(time(NULL));
-h = reclist(h);
-printlist(h);
-sortlist(h);
-printlist(h);
-freelist(h);
-return 0;
+	list_f *h = NULL;
+	srand(time(NULL));
+	h = reclist(h);
+	printlist(h);
+	sortlist(h);
+	printlist(h);
+	freelist(h);
+	return 0;
 }
 
-list_f *reclist(list_f *h){
-  if((h = malloc(sizeof(list_f))) && (i != N)){
-    h->x = rand()%N+1;
-    i++;
-    h->next = reclist(h->next);   
-  }else
-    h = NULL;
-  return h;       
+list_f *reclist(list_f *h)
+{
+	h = malloc(sizeof(list_f));
+	if ((h) && (i != N)) {
+		h->x = rand()%N+1;
+		i++;
+		h->next = reclist(h->next);
+	} else {
+		h = NULL;
+	}
+	return h;
 }
 
-void printlist(list_f *h){
-  if(h){
-    printf("\n%d", h->x);
-    return printlist(h->next);    
-  }
+void printlist(list_f *h)
+{
+	if (h) {
+		printf("\n%d", h->x);
+		return printlist(h->next);
+	}
 }
 
-void freelist(list_f *h){
-  if(h){
-    freelist(h->next);
-    free(h);
-  }
+void freelist(list_f *h)
+{
+	if (h) {
+		freelist(h->next);
+		free(h);
+	}
 }
 
-void sortlist(list_f *h){
-  list_f *temp = NULL, *tmp = NULL;
-  int swap = 1;
-  for(tmp = h->next; (tmp) && (swap); tmp = tmp->next){
-    swap = 0;
-    for(temp = h; temp->next; temp = temp->next){
-      if(temp->x > temp->next->x){
-        swap = 1;
-        i = temp->next->x;
-        temp->next->x = temp->x;
-        temp->x = i;
-      }
-    }
-  }
+void sortlist(list_f *h)
+{
+	list_f *temp = NULL, *tmp = NULL;
+	int swap = 1;
+	for (tmp = h->next; (tmp) && (swap); tmp = tmp->next) {
+		swap = 0;
+		for (temp = h; temp->next; temp = temp->next) {
+			if (temp->x > temp->next->x) {
+				swap = 1;
+				i = temp->next->x;
+				temp->next->x = temp->x;
+				temp->x = i;
+			}
+		}
+	}
 }
