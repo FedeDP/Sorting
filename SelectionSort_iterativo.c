28b4e44900d5694ea25d7f10eec49a5295603584
FedeDP <nierro92@gmail.com>
2014-04-20 18:44:48 +0200

    Updated to match linux kernel coding style

--- SelectionSort_iterativo.c
+++ SelectionSort_iterativo.c
@@ -4,10 +4,10 @@
 
 #define N 100000
 
-typedef struct list_int{
+typedef struct list_int {
   int x;
   struct list_int *next;
-}list_f;
+} list_f;
 
 list_f *reclist(list_f *h);
 void printlist(list_f *h);
@@ -16,55 +16,61 @@ void selection_sort(list_f *h);
 
 int i = 0;
 
-int main()
+int main(void)
 {
-list_f *h = NULL;
-srand(time(NULL));
-h=reclist(h);
-printlist(h);
-selection_sort(h);
-printlist(h);
-freelist(h);
-return 0;
+	list_f *h = NULL;
+	srand(time(NULL));
+	h=reclist(h);
+	printlist(h);
+	selection_sort(h);
+	printlist(h);
+	freelist(h);
+	return 0;
 }
 
-list_f *reclist(list_f *h){
-  if((h = malloc(sizeof(list_f))) && (i!=N)){
-    h->x = rand()%N+1;
-    i++;
-    h->next = reclist(h->next);   
-  }else
-    h=NULL;
-  return h;       
+list_f *reclist(list_f *h)
+{
+	h = malloc(sizeof(list_f));
+	if ((h) && (i!=N)) {
+	  h->x = rand()%N+1;
+	  i++;
+	  h->next = reclist(h->next);
+	} else {
+		h=NULL;
+	}
+	return h;
 }
 
-void printlist(list_f *h){
-  if(h){
-    printf("\n%d", h->x);
-    return printlist(h->next);    
-  }
+void printlist(list_f *h)
+{
+	if (h) {
+		printf("\n%d", h->x);
+		return printlist(h->next);
+	}
 }
 
-void freelist(list_f *h){
-  if(h){
-    freelist(h->next);
-    free(h);
-  }
+void freelist(list_f *h)
+{
+	if (h) {
+		freelist(h->next);
+		free(h);
+	}
 }
 
-void selection_sort(list_f *h){
-  list_f *temp = NULL, *min = NULL, *tmp = NULL;
-  int i;
-  for(tmp = h; tmp->next; tmp = tmp->next){
-    min = tmp;
-    for(temp = tmp->next; temp; temp = temp->next){
-      if(temp->x < min->x)
-        min = temp;
-    }
-    if(min != tmp){
-      i = tmp->x;
-      tmp->x = min->x;
-      min->x = i;
-    }
-  }
+void selection_sort(list_f *h)
+{
+	list_f *temp = NULL, *min = NULL, *tmp = NULL;
+	int i;
+	for (tmp = h; tmp->next; tmp = tmp->next) {
+		min = tmp;
+		for (temp = tmp->next; temp; temp = temp->next) {
+			if (temp->x < min->x)
+				min = temp;
+		}
+		if (min != tmp) {
+			i = tmp->x;
+			tmp->x = min->x;
+			min->x = i;
+		}
+	}
 }
